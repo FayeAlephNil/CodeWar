49d26bfba3d755b38ddfbbca90f9248182abb2df
Strikingwolf <Strikingwolf2012@gmail.com>
2015-01-31 14:38:04 -0600

    2d grid things!

--- lib/CodeWar/living/creature.rb
+++ lib/CodeWar/living/creature.rb
@@ -1,13 +1,18 @@
 module CodeWar
 module Living
 class Creature
-	def initialize(hp, atk, type, key)
+	def initialize(hp, atk, speed, type, key, x, y)
 		@hp = hp
 		@maxhp = hp
 		@atkbns = atk
+		@speed = speed
 		@type = type
 		@key = key
+		@x = x
+		@y = y
+		@goToToggle = [false, 0, 0, 0];
 	end
+
 	def attack(obj)
 		if rand(0..1) == 0 then
 			return false
@@ -16,14 +21,63 @@ class Creature
 			obj.sethp(obj.hp - atk, @key)
 		end
 	end
+
 	def hp()
 		return @hp
 	end
+
 	def sethp(arg, key)
 		if key == @key then
 			@hp = arg
 		else
 			return nil
+		end
+	end
+
+	def move(amountX, amountY)
+		@x += amountX
+		@y += amountY
+	end
+
+	def go(x, y)
+		@x = x
+		@y = y
+	end
+
+	def goTo(x, y, speed = @speed)
+		@goTo = [true, x, y, speed]
+	end
+
+	def goDirection(x, y, speed)
+		xDiff = x - @x
+		yDiff = y - @y
+
+		toX = @x + speed
+		toY = @y + speed
+
+		if (xDiff == 0)
+			toX = @x
+		end
+
+		if (xDiff < 0)
+			toX *= -1
+		end
+
+		if (yDiff == 0)
+			toY = @y
+		end
+
+		if (yDiff < 0)
+			toX *= -1
+		end
+
+		go(toX, toY);
+
+	end
+
+	def tick(time)
+		if @goTo[0]
+			goDirection(@goTo[1], @goTo[2], @goTo[3])
 		end
 	end
 end
