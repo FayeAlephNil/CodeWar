eaa266d1aed6ec6596eb4fe7ce1674b59ad7e649
Strikingwolf <Strikingwolf2012@gmail.com>
2015-02-01 17:44:16 -0600

    Implement some Tests Tests!
    Possible fix
    
    This should work

--- codewar.rb
+++ codewar.rb
@@ -1,1 +1,10 @@
-Dir["./lib/*.rb"].each {|file| require file }
+Dir[Dir.pwd + "/lib/CodeWar/util/*.rb"].each { |f| require f }
+Dir[Dir.pwd + "/lib/CodeWar/living/*.rb"].each { |f| require f }
+creature = CodeWar::Living::Creature.new()
+creature.goTo(1, 2)
+creature.tick
+creature.tick
+
+if creature.x != 1 || creature.y != 1
+  raise "GoTo does not work"
+end
